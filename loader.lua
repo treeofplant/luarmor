d53695d4ba6a2907aee171967d02ac955bcff2fc
hu <77543293+treeofplant@users.noreply.github.com>
2024-12-07 11:17:45 +0400

    Create loader.lua

---
+++ loader.lua
@@ -0,0 +1,17 @@
+
+if game.PlaceId == 10449761463 then --- TSB
+loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/1ac9b54586a547470e1dd3bdc77ee94c.lua"))()
+
+elseif game.PlaceId == 891852901 then --- GreenVille
+loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/832543db68401da3a175248c3869edf5.lua"))()
+
+elseif game.PlaceId == 15092647980 or game.PlaceId == 16200303170 then --- Project Smash
+loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/e453d35c652dc59f67581353bc5a6650.lua"))()
+
+elseif game.PlaceId == 2534724415 then --- Emergency Response: Liberty County
+loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/d8afdb07fbbcee024e2d22bfeab6abbd.lua"))()
+
+elseif game.PlaceId == 537413528 then --- Build A Boat For Treasure
+loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/bce5241ec70323b994da2a7fa9e75901.lua"))()
+
+end
